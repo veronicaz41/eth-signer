3e6794b4f7ce85cf239e43264d88acb4b25ab4bb
Joel Torstensson <oed3933@gmail.com>
2017-07-06 13:01:14 +0200

    Moved txSignerAddress to constructor and renamed signing function to signRawTx

--- test/tx_relay_signer.js
+++ test/tx_relay_signer.js
@@ -8,7 +8,7 @@ describe("SimpleSigner", function () {
   var relayAddress = "0xf85c44a9062acddcb5b174868fc62dd1f8c9e7f9";
   var txSenderAddress = "0xea674fdde714fd979de3edf0f56aa9716b898ec8";
   var rawTx = "f601850ba43b7400832fefd8949e2068cce22de4e1e80f15cb71ef435a20a3b37c880de0b6b3a7640000890abcdef012345678901c8080";
-  var signer = new TxRelaySigner(keypair, relayAddress);
+  var signer = new TxRelaySigner(keypair, relayAddress, txSenderAddress);
 
   describe("getAddress", function() {
     it("returns its address", function(done) {
@@ -30,7 +30,7 @@ describe("SimpleSigner", function () {
 
   describe("metaSignRawTx", function() {
     it("should meta sign transaction", function(done) {
-      signer.metaSignRawTx(rawTx, txSenderAddress, function(e, metaSignedRawTx) {
+      signer.signRawTx(rawTx, function(e, metaSignedRawTx) {
         // This isn't really good enough as a test, ideas are welcome
         // I think the best way to test it is to do an integration test against the txRelay
         expect(metaSignedRawTx).to.equal("f9012b80850ba43b7400832fefd894f85c44a9062acddcb5b174868fc62dd1f8c9e7f980b90104c3f44c0a000000000000000000000000000000000000000000000000000000000000001b98edc25df43bba2810ea168f8f47b19219051c11f6c713296dc2ae527d9cc8bd2ef8c6eccb6e3dfdc1b27de3635a4e482852842528ff7c15da122618347316690000000000000000000000009e2068cce22de4e1e80f15cb71ef435a20a3b37c00000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000090abcdef0123456789000000000000000000000000000000000000000000000001c8080");
@@ -42,7 +42,7 @@ describe("SimpleSigner", function () {
       var tx = new Transaction(Buffer.from(rawTx, 'hex'));
       tx.nonce = Buffer.from("");
       newRawTx = tx.serialize().toString('hex');
-      signer.metaSignRawTx(newRawTx, txSenderAddress, function(e, metaSignedRawTx) {
+      signer.signRawTx(newRawTx, function(e, metaSignedRawTx) {
         expect(e).to.equal("Nonce, to, and data have to be specified in the tx");
         expect(metaSignedRawTx).to.be.undefined;
         done();
@@ -53,7 +53,7 @@ describe("SimpleSigner", function () {
       var tx = new Transaction(Buffer.from(rawTx, 'hex'));
       tx.to = Buffer.from("");
       newRawTx = tx.serialize().toString('hex');
-      signer.metaSignRawTx(newRawTx, txSenderAddress, function(e, metaSignedRawTx) {
+      signer.signRawTx(newRawTx, function(e, metaSignedRawTx) {
         expect(e).to.equal("Nonce, to, and data have to be specified in the tx");
         expect(metaSignedRawTx).to.be.undefined;
         done();
@@ -64,7 +64,7 @@ describe("SimpleSigner", function () {
       var tx = new Transaction(Buffer.from(rawTx, 'hex'));
       tx.data = Buffer.from("");
       newRawTx = tx.serialize().toString('hex');
-      signer.metaSignRawTx(newRawTx, txSenderAddress, function(e, metaSignedRawTx) {
+      signer.signRawTx(newRawTx, function(e, metaSignedRawTx) {
         expect(e).to.equal("Nonce, to, and data have to be specified in the tx");
         expect(metaSignedRawTx).to.be.undefined;
         done();
